1f27cb1f44a3de96f244f5be546ed6377883c424
Rahul Kavale <kavale.rahul@gmail.com>
2014-10-30 18:53:56 +0530

    added Bigrams models to represent the set of bigrams starting with a word

--- src/main/scala/spike/bigrams/BigramAnalysis.scala
+++ src/main/scala/spike/bigrams/BigramAnalysis.scala
@@ -9,7 +9,23 @@ import spike.bigrams.RDDImplicits.RichRDD
 //scope for improvement by introducing domain models, using caching, hence WIP
 
 case class Bigram(firstWord: String, secondWord: String) {
+//  def merge(bigram: Bigram): Bigram = Bigram(firstWord, secondWord + "$$$$$$$$$$" + secondWord)
+
   def isValidBigram: Boolean = !firstWord.equals(" ") && !secondWord.equals(" ")
+}
+
+case class BigramsWithSameStart(firstWord: String, bigrams: List[String]) {
+  def bigramsCount = bigrams.size
+
+  def merge(bg2: BigramsWithSameStart): BigramsWithSameStart = {
+    new BigramsWithSameStart(firstWord, bigrams ++ bg2.bigrams)
+  }
+}
+
+object BigramsWithSameStart {
+  def apply(bigram: Bigram) = {
+    new BigramsWithSameStart(bigram.firstWord, List(bigram.secondWord))
+  }
 }
 
 object Bigram {
@@ -17,6 +33,7 @@ object Bigram {
     val splitWords = input.split(" ").sliding(2).toList
     splitWords.map(words => new Bigram(words(0), words(1)))
   }
+  
 }
 
 object RDDImplicits {
@@ -59,22 +76,30 @@ object BigramAnalysis {
     bgOccrCount.countWhere(a => a._2 == 1)//    296134
 
     //List the top ten most frequent bigrams and their counts.
-    val bigramsSortedByoccuranceCount = bgOccrCount.sortByDesc(a => a._2)
-    bigramsSortedByoccuranceCount.take(10)//    Array((of the,13037), (and the,7034), (the lord,7017), (in the,6738), (to the,3799), (i will,3470), (and he,3020), (shall be,3013), (all the,2714), (i have,2666))
+    val bgOccrCountSorted = bgOccrCount.sortByDesc(a => a._2)
+    bgOccrCountSorted.take(10)//    Array((of the,13037), (and the,7034), (the lord,7017), (in the,6738), (to the,3799), (i will,3470), (and he,3020), (shall be,3013), (all the,2714), (i have,2666))
 
     //  What fraction of all bigrams occurrences does the top ten bigrams account for? That is, what is the cumulative frequency of the top ten bigrams?
-    val totalBigramCount = bgOccrCount.map(a => a._2).reduce(_ + _)
-    val totalOccurancesOfTopTenBigrams = bigramsSortedByoccuranceCount.take(10).map(a => a._2).reduce((a, b) => a + b)
-    val fractionTopTenBigramOccurance = totalOccurancesOfTopTenBigrams / totalBigramCount.toFloat
+    val totalBgCount = bgOccrCount.values.reduce(_ + _)
+
+    val topTenBgOccrCount = bgOccrCountSorted.take(10).map(a => a._2).reduce((a, b) => a + b)
+
+    val fractionTopTenBigramOccurance = topTenBgOccrCount / totalBgCount.toFloat
     //  0.03327054
 
-    val startingWordBigram =  bigramsRDD.map(bg => (bg.split(" ")(0), bg))
+    val startingWordBigram =  bigramsRDD.map(bg => (bg.firstWord, bg))
 
-    val startgingWordAllBigrams = startingWordBigram.reduceByKey(_ + "$$$$$" + _).mapValues(a => a.split("$$$$$").toList)
+//        val startgingWordAllBigrams = startingWordBigram.reduceByKey(_ + "$$$$$" + _).mapValues(a => a.split("$$$$$").toList)
 
-    val startWordBGCount = startgingWordAllBigrams.map(a => (a._1, a._2.size))
+    val startgingWordAllBigrams = startingWordBigram.aggregateByKey(BigramsWithSameStart("", List()))(
+      (acc, bigram) => BigramsWithSameStart.apply(bigram),
+      (bgs1, bgs2) => bgs1.merge(bgs2))
+    
+//    val startgingWordAllBigrams = startingWordBigram.reduceByKey((bg1, bg2) => Bigrams(b1, b2)).mapValues(a => a.split("$$$$$").toList)
 
-    val startWordBGAndBGCount = bgOccrCount.map(bg => (bg._1.split(" ")(0), (bg._1, bg._2)))
+    val startWordBGCount = startgingWordAllBigrams.map(a => (a._1, a._2.bigramsCount))
+
+    val startWordBGAndBGCount = bgOccrCount.map(bg => (bg._1.firstWord, (bg._1, bg._2)))
 
     //    [(String, ((String, Int)), Int)]
     //   startWord - BG           - BG count    - bgs starting with word
