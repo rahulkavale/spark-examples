a3d7fa2e46cf71ad79768f8d3a4a7fd11ccae4b9
Rahul Kavale <kavale.rahul@gmail.com>
2014-10-30 18:13:49 +0530

    inroduced Bigram models, added some implicit methods to RichRDD, small refactorings

--- src/main/scala/spike/bigrams/BigramAnalysis.scala
+++ src/main/scala/spike/bigrams/BigramAnalysis.scala
@@ -1,38 +1,69 @@
 package spike.bigrams
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
+import spike.bigrams.RDDImplicits.RichRDD
 
 //the problem is from http://lintool.github.io/Cloud9/docs/exercises/bigrams.html
 //scope for improvement by introducing domain models, using caching, hence WIP
+
+case class Bigram(firstWord: String, secondWord: String) {
+  def isValidBigram: Boolean = !firstWord.equals(" ") && !secondWord.equals(" ")
+}
+
+object Bigram {
+  def apply(input: String): List[Bigram] = {
+    val splitWords = input.split(" ").sliding(2).toList
+    splitWords.map(words => new Bigram(words(0), words(1)))
+  }
+}
+
+object RDDImplicits {
+  implicit class RichRDD[T](rdd: RDD[T]) {
+     def countEachElement(): RDD[(T, Int)] = {
+      rdd.map(bg => (bg, 1)).reduceByKey((c1, c2) => c1 + c2)
+    }
+    
+    def countWhere(f: T => Boolean): Long = {
+      rdd.filter(f).count()
+    }
+
+    def sortByDesc[K : Ordering[K]](f: T => K): RDD[T] = {
+      val isAscending = false
+      rdd.sortBy(f, isAscending)
+    }
+  }
+  
+}
+
 object BigramAnalysis {
   def main(arg: Array[String]) = {
     val sc = new SparkContext(new SparkConf().setMaster("yarn-client").setAppName("bigram analysis"))
     val file = sc.textFile("hdfs:///tmp/bible-shakes.nopunc.gz")
-    val bigramsRDD = file.flatMap(a => getBigrams(a)).filter(a => a.trim.count(_ == ' ') == 1 )
 
-    //  How many unique bigrams are there?
-    bigramsRDD.distinct.count
-    //  432310
+    val bigramsRDD = file.flatMap(Bigram.apply).filter(_.isValidBigram)
 
-    //     total number of bigrams
-    bigramsRDD.count
-    //    1578213
+
+//     total number of bigrams
+    bigramsRDD.count //    1578213
 
     bigramsRDD.cache()
 
-    val bigramCount = bigramsRDD.map(bg => (bg, 1)).reduceByKey((c1, c2) => c1 + c2)
+//  How many unique bigrams are there?
+    bigramsRDD.distinct.count    //  432310
+
+    val bgOccrCount = bigramsRDD.countEachElement()
+    
     //    number of bigrams that appear only once:
-    bigramCount.filter(a => a._2 == 1).count
-    //    296134
+    bgOccrCount.countWhere(a => a._2 == 1)//    296134
 
     //List the top ten most frequent bigrams and their counts.
-    val bigramsSortedByoccuranceCount = bigramCount.sortBy(a => -a._2)
-    bigramsSortedByoccuranceCount.take(10)
-    //    Array((of the,13037), (and the,7034), (the lord,7017), (in the,6738), (to the,3799), (i will,3470), (and he,3020), (shall be,3013), (all the,2714), (i have,2666))
+    val bigramsSortedByoccuranceCount = bgOccrCount.sortByDesc(a => a._2)
+    bigramsSortedByoccuranceCount.take(10)//    Array((of the,13037), (and the,7034), (the lord,7017), (in the,6738), (to the,3799), (i will,3470), (and he,3020), (shall be,3013), (all the,2714), (i have,2666))
 
     //  What fraction of all bigrams occurrences does the top ten bigrams account for? That is, what is the cumulative frequency of the top ten bigrams?
-    val totalBigramCount = bigramCount.map(a => a._2).reduce(_ + _)
+    val totalBigramCount = bgOccrCount.map(a => a._2).reduce(_ + _)
     val totalOccurancesOfTopTenBigrams = bigramsSortedByoccuranceCount.take(10).map(a => a._2).reduce((a, b) => a + b)
     val fractionTopTenBigramOccurance = totalOccurancesOfTopTenBigrams / totalBigramCount.toFloat
     //  0.03327054
@@ -43,7 +74,7 @@ object BigramAnalysis {
 
     val startWordBGCount = startgingWordAllBigrams.map(a => (a._1, a._2.size))
 
-    val startWordBGAndBGCount = bigramCount.map(bg => (bg._1.split(" ")(0), (bg._1, bg._2)))
+    val startWordBGAndBGCount = bgOccrCount.map(bg => (bg._1.split(" ")(0), (bg._1, bg._2)))
 
     //    [(String, ((String, Int)), Int)]
     //   startWord - BG           - BG count    - bgs starting with word
@@ -75,5 +106,4 @@ object BigramAnalysis {
 //  2.430579E-4
 
   }
-  def getBigrams(input: String): List[String] = input.split(" ").sliding(2).toList.map(ws => ws.mkString(" "))
 }
